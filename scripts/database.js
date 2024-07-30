b9d95fd246b3105569489bad9cd51199af0f6943
Taylor Albright <jtayloralbright@gmail.com>
2024-07-30 12:36:43 -0500

    Second commit

---
+++ scripts/database.js
@@ -0,0 +1,15 @@
+export const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ]
+}
+
